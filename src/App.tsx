76f93b4a4325c6239dce13c14f70683f2184d6e5
Edécio Fernando Iepsen <edeciofernando@gmail.com>
2025-08-14 00:25:05 -0300

    v1_Layout-App-Cards

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { CardVeiculo } from "./components/CardVeiculo";
+import { InputPesquisa } from "./components/InputPesquisa";
+import type { CarroType } from "./utils/CarroType";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [carros, setCarros] = useState<CarroType[]>([])
+
+  useEffect(() => {
+    async function buscaDados() {
+      const response = await fetch("http://localhost:3000/carros")
+      const dados = await response.json()
+      console.log(dados)
+      setCarros(dados)
+    }
+    buscaDados()
+  }, [])
+
+  const listaCarros = carros.map( carro => (
+    <CardVeiculo data={carro} key={carro.id} />
+  ))
+
+  return (
+    <>
+      <InputPesquisa />
+      <div className="max-w-7xl mx-auto">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Veículos <span className="underline underline-offset-3 decoration-8 decoration-orange-400 dark:decoration-orange-600">em destaque</span>
+        </h1>
+        <div className="flex gap-3">
+          {listaCarros}
+        </div>
+      </div>
+    </>
+  );
+}
